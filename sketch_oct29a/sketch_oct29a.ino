bd25c3b049e2d032b45aeee2625c67a5eb5d14cc
Shinarthas <tttaaabbb@gmail.com>
2018-04-08 19:46:23 +0300

    stable DTW

--- sketch_oct29a/sketch_oct29a.ino
+++ sketch_oct29a/sketch_oct29a.ino
@@ -14,7 +14,7 @@ void setup(void)
 {
   pinMode(LED_BUILTIN, OUTPUT);
   //Timer1.initialize(100000);// в микросекундах
-  Timer1.initialize(100000);// в микросекундах
+  Timer1.initialize(50000);// в микросекундах
   Timer1.attachInterrupt(blinkLED); // blinkLED to run every 0.10 seconds
   Serial.begin(9600);
 }
@@ -72,9 +72,9 @@ void loop(void)
   
   //тут будет DTW
   int disctance=DTWrun(signalCopy);
-  //Serial.print(" distance ");
-  //Serial.print(disctance);
-  //Serial.println();
+  Serial.print(" distance ");
+  Serial.print(disctance);
+  Serial.println();
   signalCopy.clear();
   //delete &signalCopy;
 }
@@ -84,7 +84,7 @@ void addItemSignal(){
     curSig=255;
   }
   //Serial.println(mySignal.size());
-  while (mySignal.size()>=35){
+  while (mySignal.size()>=70){
    mySignal.pop_back();
   }
   //Serial.println(mySignal.size());
@@ -93,34 +93,33 @@ void addItemSignal(){
 }
 
 int DTWrun( QList <byte> &v) {
-  int cost;
+  int cost=0;
   byte pattern[]={52,17,54,24,62,31,68,34,69,35,66,31,63,28,60,24,60,30,67,32,64,27,63,33,71,41,82,40,70,31,60,23,58,28,68,37,74,45,93,73,121,95,136,104,140,105,138,97,122,72,97,49,78,35,64,27,64,33,76,48,82,47,77,36,65,27,61,25,62,28,68,36,72,35,70,33,66,31,66,30,65,25,59,19,51,13,50,13
-};
-  if(v.size()<30){Serial.println(v.size()); return 9999;}
-  Serial.println("here");
+  };
+  if(v.size()<10){Serial.println(v.size()); return 9999;}
+  //Serial.println("here");
   
   int len=sizeof(pattern);
   int mGamma [2][v.size()+1];
-  for( int i = 1; i <= len; i++ ) {
+  for( int i = 1; i <= v.size(); i++ ) {
     mGamma[0][i] = 0;
     mGamma[1][i] = 0;
   }
   mGamma[0][0] = 0;
   mGamma[1][0] = 0;
-  /*
-  for( int j = 1; j <= len; j++ ) {
-    for( int i = 1; i < v.size(); i++  ) {
+
+  for (int j=1; j<len; j++){
+    for(int i=1; i<=v.size();i++){
       cost = abs( v.get(i-1) - pattern[j-1] ) * abs(v.get(i-1)- pattern[j-1] );
       mGamma[1][i] = cost + min( mGamma[0][i-1], min(mGamma[0][i], mGamma[0][i-1]) );
     }
-    for( int i = 0; i < v.size(); i++  ) {
+    for( int i = 0; i <= v.size(); i++  ) {
       mGamma[0][i] = mGamma[1][i];
+      //Serial.println("wtf2");
     }
   }
-  
-  return mGamma[1][v.size()-1];
-  */
-  return 9;
+  //Serial.println(mGamma[1][v.size()]);
+  return mGamma[1][v.size()];
 }
 
 
